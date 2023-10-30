1aeb450b83ee260bfeb2350add2d44dc2869d0cb
Marc North <contact@marcnorth.co.uk>
2023-10-30 16:53:14 +0000

    Changes python version to 3.10

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="llama_grad",
     version="0.1",
-    python_requires='>3.11',
+    python_requires='>3.10',
     install_requires=[
         "torch>=2.1.0",
         "transformers>=4.34.0",
-        "html2image>=2.0.4.3"
+        "html2image>=2.0.4.3",
+        "typing-extensions>=4.8.0"
     ]
 )
