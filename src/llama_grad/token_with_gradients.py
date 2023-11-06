06a4cc7ab44e0f18fe043ed5a07f14204d03d192
Marc North <contact@marcnorth.co.uk>
2023-11-06 22:24:36 +0000

    WIP

--- src/llama_grad/token_with_gradients.py
+++ src/llama_grad/token_with_gradients.py
@@ -4,7 +4,7 @@ import torch
 
 
 class TokenWithGradients:
-    def __init__(self, device: torch.device):
+    def __init__(self, device: torch.device = torch.device("cpu")):
         # 1-D Tensor of predicted token_ids of size (output_sequence_length)
         self.token_ids: Tensor = torch.zeros([0], dtype=torch.int).to(device)
         # 2-D Tensor of gradients of size (output_sequence_length, input_size + self.token_ids.shape[0] - 1)
@@ -15,7 +15,7 @@ class TokenWithGradients:
             self.token_ids = other.token_ids.clone()
             self.gradients = other.gradients.clone()
         else:
-            zeros = torch.zeros((self.gradients.shape[0], other.gradients.shape[1] - self.gradients.shape[1]))
+            zeros = torch.zeros((self.gradients.shape[0], other.gradients.shape[1] - self.gradients.shape[1])).to(self.gradients.device)
             self.gradients = torch.cat([self.gradients, zeros], 1)
             self.token_ids = torch.cat((self.token_ids, other.token_ids), 0)
             self.gradients = torch.cat((self.gradients, other.gradients), 0)
